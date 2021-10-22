568c5f70bcb4f12d183445b0e36b03ebba610a0d
Aliona <alynastempel@gmail.com>
2021-10-22 22:19:15 +0300

    initial project version

---
+++ zamena.py
@@ -0,0 +1,3 @@
+s = str(input('Введите необходимый текст: '))
+print(s.replace('Uncle Styopa', 'Aliona').replace('uncle Styopa', 'Aliona').replace('Styopa', 'Aliona').replace('uncle', 'Aliona').replace('Uncle', 'Aliona'))
+
